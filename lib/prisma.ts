bb7aba0791d1206269a3b54fe923cb26660ac5a1
Flowskyy <flowskyyy@gmail.com>
2026-07-13 11:15:58 +0700

    feat: integrate Prisma ORM with Accelerate extension and define initial User schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '.prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: ReturnType<typeof makePrismaClient> | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+function makePrismaClient() {
+  return new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? makePrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
